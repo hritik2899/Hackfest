7f4fa5bd336a04cddff90cadbf160639e0bca288
hritikgupta426 <hritilohiya46@gmail.com>
2023-04-13 16:14:36 +0530

    minor changes

--- app.js
+++ app.js
@@ -41,4 +41,8 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+
+app.listen(3000, () => {
+  console.log("Listening on port  8080");
+})
+// module.exports = app;
